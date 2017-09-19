af9930a0f12d7cd2b96e2f017813800c6174f546
Julian Mariño <richardes71@gmail.com>
2017-09-19 16:36:27 -0300

    sb

--- JulianMicroRTS/src/dynamicscripting/DynamicScripting.java
+++ JulianMicroRTS/src/dynamicscripting/DynamicScripting.java
@@ -57,7 +57,7 @@ public class DynamicScripting extends AIWithComputationBudget {
 	AuxMethods aux=new AuxMethods();
 	UnitStatistics unitStatistics;
 	int nplayouts = 0;
-	int LOOKAHEAD = 2000;
+	int LOOKAHEAD = 3000;
 	EvaluationFunction evaluation = null;
 	int initialWeight=100;
 	Unit [] maxUnits;
