d334e25c738eead74e27f065f7b7f699c7e4b246
Julian Mariño <richardes71@gmail.com>
2017-12-18 15:47:57 -0200

    sbf

--- JulianMicroRTS/src/dynamicscripting/DynamicScripting.java
+++ JulianMicroRTS/src/dynamicscripting/DynamicScripting.java
@@ -75,7 +75,7 @@ public class DynamicScripting extends AIWithComputationBudget {
 	int roundConvergenceDraw=-1;
 	int limitConvergence=8;
 	int enemyIA;
-	int totalIterationsDS=300;
+	int totalIterationsDS=100;
 	int startCompoundScripts=0;
 	private ArrayList<CompoundScript> [] bestCompoundScript;
 	int bestScriptsLimit=30;
